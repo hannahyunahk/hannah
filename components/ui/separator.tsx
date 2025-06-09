9b9cd96ec151a41c74f1cc126d08e8c7ed618b6e
hannah <hannahyunah@gmail.com>
2025-06-09 20:06:44 +0900

    Separator 타입 에러 수정

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,22 +1,26 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-const Separator = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
-  <div
-    ref={ref}
-    role={decorative ? "separator" : undefined}
-    aria-orientation={orientation}
-    className={cn(
-      "shrink-0 bg-border",
-      orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-      className
-    )}
-    {...props}
-  />
-))
+interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
+  orientation?: "horizontal" | "vertical"
+  decorative?: boolean
+}
+
+const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
+  ({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
+    <div
+      ref={ref}
+      role={decorative ? "separator" : undefined}
+      aria-orientation={orientation}
+      className={cn(
+        "shrink-0 bg-border",
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        className
+      )}
+      {...props}
+    />
+  )
+)
 Separator.displayName = "Separator"
 
-export { Separator } 
+export { Separator }
